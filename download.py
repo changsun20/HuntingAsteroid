5d81d7b0d85aae8564b501418962a8da77712eee
changsun20 <3370518463@qq.com>
2023-09-03 15:11:19 +0800

    update the python file based on the jupyter file

--- download.py
+++ download.py
@@ -1,9 +1,11 @@
+import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 import matplotlib.pyplot as plt
 from astroquery.mast import Observations
 import getpass
 import os
+import shutil
 
 
 # read the FITS file, return the data and WCS object
@@ -23,6 +25,8 @@ def get_par(line):
     dec1 = float(line[115:126])
     ra2 = float(line[127:138])
     dec2 = float(line[139:150])
+
+    print(obsID)
     return obsID, ra1, dec1, ra2, dec2
 
 
@@ -63,24 +67,91 @@ def download_file(obsID):
                                               productSubGroupDescription='DRC',
                                               obs_collection='HST')
 
+    # Generate the path for the FITS file
+    path = '.\mastDownload\HST\{}\{}_drc.fits'.format(obsID, obsID)
+
+    # Check if the FITS file exists
+    if os.path.isfile(path) == False:
+        print('Error: No file found at obsID {}'.format(obsID))
+
+    return path
+
+
+# Save the data and asteroid position into a npz file
+def save_data(obsID, index, data, x1, y1, x2, y2):
+    matrix2 = np.array([x1, y1, x2, y2])
+    dataPath = '.\data\{}-{}'.format(index, obsID)
+    np.savez(dataPath, matrix1=data, matrix2=matrix2)
+    print('Successfully save data for {}-{}'.format(index, obsID))
+
+
+# Sometimes there may be multiple asteroids in the same FITS file,
+# so we'd love to delete the FITS file several iterations after
+# the inital download.
+class FileManager:
+    def __init__(self, capacity):
+        self.list = []
+        self.capacity = capacity
+
+    def update_list(self, nextID):
+        self.list.append(nextID)
+        if len(self.list) <= self.capacity:
+            return 0
+        else:
+            return self.list.pop(0)
+
+    def get_list(self):
+        print(self.list)
+
+
+# Delete the unwanted directory, if the directory still exists
+def delete_dir(fileNameDelete):
+    directory_path = '.\mastDownload\HST\{}'.format(fileNameDelete)
+    print('Preparing to delete {}'.format(directory_path))
+
+    if os.path.exists(directory_path):
+        shutil.rmtree(directory_path)
+        print("{} is deleted!".format(directory_path))
+    else:
+        print("Warning: {} has already been deleted".format(directory_path))
+
 
 if __name__ == '__main__':
     connect_session()
+
     with open('tablea1.dat', 'r') as file:
-        num = 1
-        for line in file:
+        # Generate a FileManager object, and configure the number of FITS file
+        # you want to store at one time on your disk
+        manager = FileManager(8)
+
+        for index, line in enumerate(file):
+            # Get the parameter (obsID and asteroid position)
             obsID, ra1, dec1, ra2, dec2 = get_par(line)
-            print(obsID)
-            download_file(obsID)
-            path = '.\mastDownload\HST\{}\{}_drc.fits'.format(obsID, obsID)
-            if os.path.isfile(path) == False:
-                print('Error: No file found at obsID {}'.format(obsID))
-                continue
+
+            # Download FITS file, and get the path
+            path = download_file(obsID)
+
+            # Acquire the data, and convert the coordinate system for asteroid position
             data, x1, y1, x2, y2 = convert_world2pix(
                 path, ra1, dec1, ra2, dec2)
-            plot_data(data, obsID, num, x1, y1, x2, y2)
+
+            # Save the data and position to .npz file
+            save_data(obsID, index, data, x1, y1, x2, y2)
+
+            # Plot the data and save the figure to a jpg file
+            plot_data(data, obsID, index, x1, y1, x2, y2)
+
+            # Check which file to delete in this iteration
+            fileNameDelete = manager.update_list(obsID)
+
+            # Delete the directory if expected
+            if fileNameDelete != 0:
+                delete_dir(fileNameDelete)
+
+            # Finished!
             print('{} finished!'.format(obsID))
-            if num > 5:
+
+            # Stop the code
+            if index > 5:
                 break
-            num += 1
     file.close()
