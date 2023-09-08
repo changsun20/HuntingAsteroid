39d010b6762a20b9c5e2a3ee66e6cc7ed646c241
changsun20 <3370518463@qq.com>
2023-09-08 20:09:49 +0800

    minor change to negativepic file

---
+++ negative.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import preprocess as prep
+
+
+def corner_point(position):
+    x1, y1, x2, y2 = position
+    return int(min(x1, x2)), int(min(y1, y2))
+
+
+def data_refresh(data, x_final, y_final, window_shape_value):
+    data_refresh = data[y_final:y_final+window_shape_value,
+                        x_final:x_final+window_shape_value]
+    return data_refresh
+
+
+# Save the data and asteroid position into a npz file
+def save_data_negative(name, index, count, data, data_dir):
+    dataPath = '{}\{}_{}'.format(data_dir, count, name)
+    np.savez(dataPath, matrix1=data)
+    print('Successfully save data for index:{} count:{} filename:{}'.format(
+        index, count, name))
+
+
+def plot_data_negative(name, count, data, pic_dir):
+    plt.imshow(data, cmap='gray', vmin=0, vmax=0.01)
+    plt.colorbar()
+    plt.savefig('{}\{}_{}.jpg'.format(pic_dir, count, name), dpi=300)
+    plt.clf()
+
+
+def generate_negative_data(father_dir, pic_dir, data_dir, start_index, end_index, window_shape_value=1000):
+    count = 0
+
+    for index in range(start_index, end_index+1):
+        path, name = prep.path_of_data(father_dir, index)
+        print('Start preprocessing {}: {}'.format(index, name))
+        data, position = prep.load_saved_data(path)
+        data = np.nan_to_num(data)
+        x_corner, y_corner = corner_point(position)
+        if x_corner > window_shape_value+500 and y_corner > window_shape_value+500:
+            for i in range(2):
+                x_final = random.randint(0, x_corner - window_shape_value)
+                y_final = random.randint(0, y_corner - window_shape_value)
+                data_new = data_refresh(
+                    data, x_final, y_final, window_shape_value)
+                save_data_negative(name, index, count, data_new, data_dir)
+                plot_data_negative(name, count, data_new, pic_dir)
+                count += 1
+
+
+if __name__ == '__main__':
+    generate_negative_data('.\data',
+                           '.\pic_negative',
+                           '.\\preprocess\\train\\negative',
+                           0, 10, 1000)
